f1067ba5decad4ea50e686fc1e9e4180a3cda419
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2023-11-03 15:37:44 +0200

    Add project files.

---
+++ WebTicket/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebTicket.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
